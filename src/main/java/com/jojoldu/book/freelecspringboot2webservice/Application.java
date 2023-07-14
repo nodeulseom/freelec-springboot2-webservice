74e1d0acd6fb714c7acf25218071d43d15d8cd5b
junghyun <hyunnov04@gmail.com>
2023-07-14 10:38:08 +0900

    commit

--- src/main/java/com/jojoldu/book/freelecspringboot2webservice/Application.java
+++ src/main/java/com/jojoldu/book/freelecspringboot2webservice/Application.java
@@ -2,12 +2,14 @@ package com.jojoldu.book.freelecspringboot2webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
-public class FreelecSpringboot2WebserviceApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(FreelecSpringboot2WebserviceApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
